93a24cdfc62c7fc6d0fa124981ae2ec14e5d42cc
tnsej <tnsej@LAPTOP-JASVGN56>
2023-03-20 09:02:35 +0900

    03

--- StartWalk/src/main/java/co/start/controller/LoginControl.java
+++ StartWalk/src/main/java/co/start/controller/LoginControl.java
@@ -16,23 +16,27 @@ public class LoginControl implements Control {
 	@Override
 	public String exec(HttpServletRequest req, HttpServletResponse resp) {
 		// TODO Auto-generated method stub
-		String id = req.getParameter("userid");
+		String id = req.getParameter("userId");
+		String pw = req.getParameter("userPasswd");
 
 		UserVO vo = new UserVO();
 		
 		UserService service = new UserServiceMybatis();
 		
 		vo = service.login(id);
+		vo = service.login(pw);
+		System.out.println(vo);
+		
+		// 조회 비교
+		
 		HttpSession session = req.getSession(); // 요청정보에 있는 세션정보를 있으면 호출. 없으면 세션 객체생성.
 		if(vo!=null) {
-			session.setAttribute("id", id);
-		session.setAttribute("user", vo);
-		req.setAttribute("message", id+"님의 로그인을 환영합니다.");
+			session.setAttribute("userId", id);
+			//session.setAttribute("userId", vo);
+			req.setAttribute("message", id+"님의 로그인을 환영합니다.");
 		}else {
 			req.setAttribute("message", "존재하지 않는 아이디입니다.");
 		}
-		System.out.println(vo);
-		System.out.println("after: " + vo);
 
 		// session 객체는 웹브라우저별로 계속 유지되는 정보를 담아놓기 좋은 객체.
 		
--- StartWalk/src/main/java/co/start/controller/PackageInfoControl.java
+++ StartWalk/src/main/java/co/start/controller/PackageInfoControl.java
@@ -1,14 +1,27 @@
 package co.start.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import co.start.common.Control;
+import co.start.service.ProductService;
+import co.start.service.ProductServiceMybatis;
+import co.start.vo.ProductVO;
 
 public class PackageInfoControl implements Control {
 
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse response) {
+		String code = request.getParameter("pdId");
+		ProductService service = new ProductServiceMybatis();
+		
+		ProductVO vo = service.packageInfo(Integer.parseInt(code));
+		
+		System.out.println(vo);
+		
+		request.setAttribute("info",vo);
 		
 		return "product/packageInfo.tiles";
 	}
