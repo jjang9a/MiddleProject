c0e71d7777b0bc28fc2c9bfb560186773a21d3d6
YOUNG <ddi061205@naver.com>
2023-03-24 17:53:56 +0900

    0324 jun

--- StartWalk/src/main/java/co/start/controller/ScUserWriteControl.java
+++ StartWalk/src/main/java/co/start/controller/ScUserWriteControl.java
@@ -4,13 +4,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import co.start.common.Control;
+import co.start.service.BoardService;
+import co.start.service.BoardServiceMybatis;
+import co.start.vo.BoardVO;
 
 public class ScUserWriteControl implements Control {
 
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String userId = "admin";
+		String title = request.getParameter("title") ;
+		String contents = request.getParameter("text");
+		
+		BoardVO vo = new BoardVO();
+		vo.setUserId(userId);
+		vo.setBTitle(title);
+		vo.setBContents(contents);
+		
+		System.out.println(vo);
+		
+		BoardService service = new BoardServiceMybatis();
+		service.UserWrite(vo);
+		
+		return "scUserList.do";
 	}
 
 }
